b504bb7a7ec8a83e4f168cb696ed61116475af52
sanamd1621-lang <sanamd1621@gmail.com>
2026-08-08 17:03:49 +0530

    add username column to users table

--- init_db.py
+++ init_db.py
@@ -10,7 +10,8 @@ def create_tables():
         CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             full_name TEXT NOT NULL,
-            email TEXT UNIQUE NOT NULL,
+            username TEST UNIQUE NOT NULL,
+            email TEXT NOT NULL,
             password TEXT NOT NULL,
             phone TEXT,
             role TEXT DEFAULT 'user'
